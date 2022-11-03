3ac72ea90e0a5472b15807925136f07b5092eea6
Дмитрий <Skrevo@outlook.com>
2022-11-03 19:49:04 +0200

    Init START

--- src/main/java/com/example/clientsservice/ClientsServiceApplication.java
+++ src/main/java/com/example/clientsservice/ClientsServiceApplication.java
@@ -8,6 +8,7 @@ public class ClientsServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientsServiceApplication.class, args);
+        System.out.println("START");
     }
 
 }
