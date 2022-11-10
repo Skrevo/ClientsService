86345d4478e5768fbd32073397d8635348ab682d
Дмитрий <Skrevo@outlook.com>
2022-11-10 20:53:23 +0200

    repositories tests Client Phone

--- src/test/java/com/example/clientsservice/ClientsServiceApplicationTests.java
+++ src/test/java/com/example/clientsservice/ClientsServiceApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.clientsservice;
 
+import com.example.clientsservice.repositories.ClientRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ClientsServiceApplicationTests {
-
+    @Autowired
+    private ClientRepository clientRepository;
     @Test
     void contextLoads() {
+        //Assertions.assertNull(clientRepository,"Not Null!!!");
+        Assertions.assertNotNull(clientRepository,() -> String.format("%s",111));
     }
 
 }
