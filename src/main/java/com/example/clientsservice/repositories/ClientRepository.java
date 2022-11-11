24de195c7e8a750a9ecac8785a8f9adb3b6a27e9
Дмитрий <Skrevo@outlook.com>
2022-11-11 14:33:03 +0200

    db_services_dz Repositories

--- src/main/java/com/example/clientsservice/repositories/ClientRepository.java
+++ src/main/java/com/example/clientsservice/repositories/ClientRepository.java
@@ -2,11 +2,9 @@ package com.example.clientsservice.repositories;
 
 import com.example.clientsservice.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-//@Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
     List<Client> findAllBySurnameAndNameAndPatronymic(String surname, String name, String patronymic);
 
--- src/main/java/com/example/clientsservice/repositories/PhoneRepository.java
+++ src/main/java/com/example/clientsservice/repositories/PhoneRepository.java
@@ -1,6 +1,5 @@
 package com.example.clientsservice.repositories;
 
-import com.example.clientsservice.models.Client;
 import com.example.clientsservice.models.Phone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
