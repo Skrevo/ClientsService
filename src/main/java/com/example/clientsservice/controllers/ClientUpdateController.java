739518f9d1509d8d38c193188267dc847625ad60
Дмитрий <Skrevo@outlook.com>
2022-11-22 21:37:50 +0200

    mustache clientUpdateForm

--- src/main/java/com/example/clientsservice/controllers/ClientUpdateController.java
+++ src/main/java/com/example/clientsservice/controllers/ClientUpdateController.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -34,5 +37,15 @@ public class ClientUpdateController {
         }
         model.addAttribute("genders", genders.entrySet());
         return "clientUpdate";
+    }
+
+    @PostMapping("clientUpdateForm")
+    public ModelAndView clientUpdateForm(@ModelAttribute("client")Client client) {
+        Client dbClient = clientsService.findById(client.getId());
+        client.setAddress(dbClient.getAddress());
+        System.err.println(dbClient.getAddress());
+        clientsService.save(client);
+        return new ModelAndView("redirect:clientUpdate",
+                new ModelMap("clientId", client.getId()));
     }
 }
