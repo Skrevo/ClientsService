7d6c39e7760d0e35b25689cdb8ba3e3221ab0ce7
Дмитрий <Skrevo@outlook.com>
2022-11-22 21:04:33 +0200

    mustache clientUpdate genders Auto

--- src/main/java/com/example/clientsservice/controllers/ClientUpdateController.java
+++ src/main/java/com/example/clientsservice/controllers/ClientUpdateController.java
@@ -5,8 +5,14 @@ import com.example.clientsservice.services.data.ClientsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.clientsservice.models.Client.*;
 
 @Controller
 public class ClientUpdateController {
@@ -16,6 +22,17 @@ public class ClientUpdateController {
     public String load(@RequestParam("clientId") Integer id, Model model) {
         Client client = clientsService.findById(id);
         model.addAttribute("client", client);
+        /*
+        for (Gender value : Gender.values()) {
+            model.addAttribute(value.name(), value == client.getGender() ? "selected" : "");
+        }
+
+         */
+        ModelMap genders = new ModelMap();
+        for (Gender value : Gender.values()) {
+            genders.put(value.name(), value == client.getGender() ? "selected" : "");
+        }
+        model.addAttribute("genders", genders.entrySet());
         return "clientUpdate";
     }
 }
